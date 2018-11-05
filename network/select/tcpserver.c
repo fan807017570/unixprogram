22b8858f14cc31f07bac2afad48172803df61ca5
fan807017570 <807017570@qq.com>
2018-11-05 23:26:07 +0800

    补充select模型服务端代码

--- network/select/tcpserver.c
+++ network/select/tcpserver.c
@@ -10,28 +10,34 @@
 #include<sys/time.h>
 #include<sys/socket.h>
 #include<sys/types.h>
+#include<string.h>
+#include<sys/select.h>
+#include<unistd.h>
 #define SERVER_PORT 8090
 #define     TRUE    1 
 #define     FALSE   0
+#define     BUF_LEN 100
 int main(int argc ,char ** argv){
     int sfd,max_sd=-1,cfd;
     int on=1,rc,desc_ready=-1,i;
+    int close_conn;
+    char buffer[BUF_LEN];
     fd_set master_set ,working_set;
     struct timeval timeout;
     struct sockaddr_in saddr,cliaddr;
     sfd=socket(AF_INET,SOCK_STREAM,0);
+    if(sfd<0){
+        printf("socket failed error:%s",strerror(errno));
+        return -1;
+    }
     rc=ioctl(sfd,FIONBIO,(char *)&on);
     if(rc<0){
-        perror("ioctl() failed:%s",strerror(errno));
+        printf("ioctl() failed :%s",strerror(errno));
         return -1;
     }
-    rc = setsockopt(sfd,SOL_SOCKET,SO_REUSEADDR,(char*)&on,sizeof(on))
+    rc = setsockopt(sfd,SOL_SOCKET,SO_REUSEADDR,(char*)&on,sizeof(on));
     if(rc<0){
-        perror("setsockopt failed:%s\n",strerror(errno));
-        return -1;
-    }
-    if(sfd<0){
-        perror("socket() failed :%s\n",strerror(errno));
+        printf("setsockopt failed:%s\n",strerror(errno));
         return -1;
     }
     memset(&saddr,0,sizeof(struct sockaddr_in));
@@ -40,26 +46,26 @@ int main(int argc ,char ** argv){
     saddr.sin_port=htons(SERVER_PORT);
     rc =bind(sfd,(struct sockaddr *)&saddr,sizeof(struct sockaddr));
     if(rc!=0){
-        perror("bind error :%s \n",strerror(errno));
+        printf("bind error :%s \n",strerror(errno));
         close(sfd);
         return -1;
     }
     rc=listen(sfd,10);
     if(rc==-1){
-        perror("listen error :%s \n",strerror(errno));
+        printf("listen error :%s \n",strerror(errno));
         close(sfd);
         return -1;
     }
     FD_ZERO(&master_set);
     max_sd=sfd;
-    FD_SET(SFD,&master_set);
+    FD_SET(sfd,&master_set);
     timeout.tv_sec=3*60;
     timeout.tv_usec=0;
     do{
         memcpy(&working_set,&master_set,sizeof(master_set));
         rc=select(max_sd+1,&working_set,NULL,NULL,&timeout);
         if(rc<0){
-            perror("select error:%s\n",strerror(errno));
+            printf("select error:%s\n",strerror(errno));
             close(sfd);
             break;
         }
@@ -69,29 +75,57 @@ int main(int argc ,char ** argv){
         }
         desc_ready=rc;
         for(i=0;i<max_sd && desc_ready>0;++i){
-            if(FD_ISSET(i,working_set)){
-                desc_ready--;
-                if(i==sfd){
-                    //listen descriptor  is ready
-                    cfd=accept(sfd,NULL,NULl);
-                    if(cfd<0){
-                        perror("accept faild :%s \n",strerror(errno));
-                        close(sfd);
-                        break;
-                        // return -1;
-                    }else{
+            if(FD_ISSET(i,&working_set)){
+                    if(i==sfd){
+                        do{
+                            cfd=accept(sfd,NULL,NULL);
+                            if(cfd<0){
+                                printf("accept error:%s \n",strerror(errno));
+                                break;
+                            }
+                            FD_SET(cfd,&master_set);
+                            if(cfd>max_sd)
+                            {
+                                max_sd=cfd;
+                            }
+                            desc_ready--;
+                        }while(cfd!=-1);
+                    }
+                else{
+                    // other descriptor is ready
+                    close_conn=FALSE;
+                    do{
+                        rc=recv(i,buffer,sizeof(buffer),0);
+                        if(rc<0){
+                            if(errno!=EWOULDBLOCK){
+                                printf("recv error:%s\n",strerror(errno));
+                                close_conn=TRUE;
+                            }
+                            break;
+                        }
+                        if(rc==0){
+                            printf("connection closed");
+                            close_conn=TRUE;
+                            break;
+                        }
+                        int len=rc;
+                        printf("recv %d bytes\n",rc);
+                        buffer[rc]='\n';
+                        printf("recv data is :%s \n",buffer);
+                        rc=send(i,buffer,len,0);
+                        if(rc<0){
+                            printf("send data error:%s\n",strerror(errno));
+                            close_conn=TRUE;
+                            break;
+                        }
+                    }while(TRUE);
+                    if(close_conn==TRUE){
+                        close(i);
+                        FD_CLR(i,&master_set);
 
                     }
-
-                }else{
-                    // other descriptor is ready
-
                 }
             }
-
         }
-
-
-    }while()
-    
+    }while(TRUE);
 }
